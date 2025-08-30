5b86a1614db4928a033490ae2df12d92792c789f
Juliana Faria <juli-moreira@hotmail.com>
2025-08-30 10:13:05 -0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import {Routes} from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+export const routes: Routes = [
+  {
+    path: "login",
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)  // ✅ certo
+  }
+];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from 'src/app/components/default-login-layout/default-login-layout.component';
+
+@Component({
+  standalone: true,
+  selector: 'app-login',
+  imports: [
+    DefaultLoginLayoutComponent
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+ })
+export class LoginComponent {
+
+}
